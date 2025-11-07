c1d806540d74585a1df8081312cb5c4ec81da107
Ian-Pasia01 <ianjaypasia@gmai.com>
2025-11-07 21:59:42 +0800

    feat/create-table-medicalrecords

--- veterinary.sql
+++ veterinary.sql
@@ -42,3 +42,13 @@ CREATE TABLE invoices (
     invoicedate DATE,
     FOREIGN KEY (appointid) REFERENCES appointments(appointid)
 );
+CREATE TABLE medicalrecords (
+    recordid INT PRIMARY KEY,
+    animalid INT,
+    doctorid INT,
+    diagnosis VARCHAR(255),
+    treatment VARCHAR(255),
+    visitdate DATE,
+    FOREIGN KEY (animalid) REFERENCES animals(animalid),
+    FOREIGN KEY (doctorid) REFERENCES doctors(doctorid)
+);
